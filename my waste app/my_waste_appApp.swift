214e5ff73b936b5f17f9cac3a4a2331917d132e0
oceaniswater <markflyingbear@gmail.com>
2023-07-24 13:08:34 +0100

    Initial Commit

---
+++ my waste app/my_waste_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_waste_appApp.swift
+//  my waste app
+//
+//  Created by Mark Golubev on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct my_waste_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
