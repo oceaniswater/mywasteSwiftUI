8a71e9995ea782a90a5bad4263dd2e72294d4601
oceaniswater <markflyingbear@gmail.com>
2023-07-24 21:59:29 +0100

    create singleton for test

--- my waste app/Singleton.swift
+++ my waste app/Singleton.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+final class Singleton {
+    static let shared = Singleton()
+    private init() {}
+    private var bins = [Bin(color: .red, type: .glass, days: ["Mon"]), Bin(color: .green, type: .general, days: ["Tue", "Fri"]), Bin(color: .blue, type: .paper, days: ["Mon"] ), Bin(color: .green, type: .metal, days: ["Tue", "Fri"])]
+    func getBins() -> [Bin] {
+        return bins
+    }
+}
