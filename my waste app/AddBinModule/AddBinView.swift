f00ada6992e3000aa715856e9031e7c4053ad658
oceaniswater <markflyingbear@gmail.com>
2023-07-24 21:59:48 +0100

    add AddBinView

--- my waste app/AddBinModule/AddBinView.swift
+++ my waste app/AddBinModule/AddBinView.swift
@@ -8,13 +8,70 @@
 import SwiftUI
 
 struct AddBinView: View {
+    
+    @State var colorSelected: BinColor = .red
+    @State var typeSelected: BinType = .glass
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color("primary_bg")
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    ImageBin(colorSelected: $colorSelected)
+                    ColorPicker(colorSelected: $colorSelected)
+                    TypePicker(typeSelected: $typeSelected)
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
 struct AddBinView_Previews: PreviewProvider {
     static var previews: some View {
         AddBinView()
+    }
+}
+
+struct ImageBin: View {
+    @Binding var colorSelected: BinColor
+    var body: some View {
+        Image(colorSelected.rawValue)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(maxHeight: 200)
+            .padding()
+    }
+}
+
+struct ColorPicker: View {
+    @Binding var colorSelected: BinColor
+    var body: some View {
+        List {
+            Picker("Color", selection: $colorSelected) {
+                ForEach(BinColor.allCases) { bin in
+                    Text(bin.rawValue)
+                }
+            }
+        }
+        .scrollContentBackground(.hidden)
+        .pickerStyle(.wheel)
+    }
+}
+
+struct TypePicker: View {
+    @Binding var typeSelected: BinType
+    var body: some View {
+        List {
+            Picker("Type", selection: $typeSelected) {
+                ForEach(BinType.allCases) { bin in
+                    Text(bin.rawValue)
+                }
+            }
+        }
+        .scrollContentBackground(.hidden)
+        .clipped()
+        .pickerStyle(.automatic)
     }
 }
