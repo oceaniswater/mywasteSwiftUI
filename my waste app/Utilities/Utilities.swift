5d68c565c50349be737efcc93b4ebcb8c9f647d5
oceaniswater <markflyingbear@gmail.com>
2023-08-10 18:18:28 +0100

    fv

--- my waste app/Utilities/Utilities.swift
+++ my waste app/Utilities/Utilities.swift
@@ -5,27 +5,27 @@
 //  Created by Mark Golubev on 31/07/2023.
 //
 
-import Foundation
-import UIKit
-
-final class Utilities {
-    static let shared = Utilities()
-    private init() {}
-    
-    @MainActor
-    func topViewController(controller: UIViewController? = nil) -> UIViewController? {
-        let controller = controller ?? UIApplication.shared.keyWindow?.rootViewController
-        if let navigationController = controller as? UINavigationController {
-            return topViewController(controller: navigationController.visibleViewController)
-        }
-        if let tabController = controller as? UITabBarController {
-            if let selected = tabController.selectedViewController {
-                return topViewController(controller: selected)
-            }
-        }
-        if let presented = controller?.presentedViewController {
-            return topViewController(controller: presented)
-        }
-        return controller
-    }
-}
+//import Foundation
+//import UIKit
+//
+//final class Utilities {
+//    static let shared = Utilities()
+//    private init() {}
+//    
+//    @MainActor
+//    func topViewController(controller: UIViewController? = nil) -> UIViewController? {
+//        let controller = controller ?? UIApplication.shared.keyWindow?.rootViewController
+//        if let navigationController = controller as? UINavigationController {
+//            return topViewController(controller: navigationController.visibleViewController)
+//        }
+//        if let tabController = controller as? UITabBarController {
+//            if let selected = tabController.selectedViewController {
+//                return topViewController(controller: selected)
+//            }
+//        }
+//        if let presented = controller?.presentedViewController {
+//            return topViewController(controller: presented)
+//        }
+//        return controller
+//    }
+//}
